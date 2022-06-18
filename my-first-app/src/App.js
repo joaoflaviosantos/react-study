5d7eda696ca296a305a521c1cfbfacf72c1cbfe0
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-06-17 23:32:40 -0300

    feat: implementing youtube-hora-de-codar-react-03 instructions

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const name = 'João Flávio'
+
+  const nameUpercase =  name.toUpperCase()
+
+  function sumFunction(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Hello React!</h1>
-      <p>My Fisrt React App</p>
+      <p>My First React App</p>
+      <p>Author: { name } | { nameUpercase }</p>
+      <p>Sum: { 2 + 2 } | { sumFunction(2, 2) }</p>
+      <img src={url} alt="placeholder" />
     </div>
   );
 }
