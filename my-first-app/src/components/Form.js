fefef63dd933920552f65276d2c1857482a89663
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-06-18 02:12:50 -0300

    feat: implementing youtube-hora-de-codar-react-10 (useState)

--- my-first-app/src/components/Form.js
+++ my-first-app/src/components/Form.js
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 function Form() {
+
+    const [ name, setName ] = useState()
+    const [ password, setPassword ] = useState()
 
     function registerUser(e) {
         e.preventDefault();
-        alert(`Registered user future binded input!`)
+        console.log(`User ${name} was registered with the password ${password}.`)
     }
-    
+
     return (
         <div>
             <h1>My Form</h1>
             <form onSubmit={ registerUser }>
                 <div>
-                    <input type="text" placeholder="Input your name" />
+                    <label htmlFor="name">Name: </label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Input your name"
+                    onChange={ (e) => setName(e.target.value) } />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    placeholder="Input your password"
+                    onChange={ (e) => setPassword(e.target.value) } />
                 </div>
                 <div>
                     <input type="submit" value="Send" />
